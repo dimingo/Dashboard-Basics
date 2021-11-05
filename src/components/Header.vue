e57dbd5896acf40886aff1fd34598f79793de7ca
mitchel Ketcha <micalvin98@gmail.com>
2021-11-05 21:38:57 +0300

    initial Commit

---
+++ src/components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+
+    <v-app-bar app
+      fixed
+      color="white"
+      elevate-on-scroll
+      
+        elevation="4"
+    >
+  
+
+      <v-toolbar-title>Title</v-toolbar-title>
+       <v-app-bar-nav-icon @click="$emit('handleDrawer')"></v-app-bar-nav-icon>
+
+      <v-spacer></v-spacer>
+   <v-col lg="6" cols="12">
+         <v-form class="mt-5" >
+             <v-text-field rounded outlined dense append-icon="mdi-magnify" placeholder="Search"></v-text-field>
+         </v-form>
+     </v-col>
+     
+    
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+</template>
+<script>
+export default {
+    setup() {
+        
+    },
+}
+</script>
+<style scoped>
+
+</style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-navigation-drawer v-model="drawer" app>
+    <v-sheet color="blue lighten-4" class="pa-4">
+      <div class="text-center">
+      <v-avatar class="mb-4" color="grey darken-1" size="64"></v-avatar>
+
+      <div>john@vuetifyjs.com</div>
+      </div>
+    </v-sheet>
+
+    <v-divider></v-divider>
+
+    <v-list>
+      <v-list-item v-for="[icon, text] in links" :key="icon" link>
+        <v-list-item-icon>
+          <v-icon>{{ icon }}</v-icon>
+        </v-list-item-icon>
+
+        <v-list-item-content>
+          <v-list-item-title>{{ text }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+<script>
+export default {
+  props: ["drawer"],
+  name: "sidebar",
+  data() {
+    return {
+      links: [
+        ["mdi-inbox-arrow-down", "Inbox"],
+        ["mdi-send", "Send"],
+        ["mdi-delete", "Trash"],
+        ["mdi-alert-octagon", "Spam"],
+      ],
+    };
+  },
+};
+</script>
+<style scoped></style>
